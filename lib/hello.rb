7972c6494fc7da0b77add1606a09f6d16f03f16b
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 19:12:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(names)
+  i = 0
+  while i < names.length
+    yield names[i]
+    i += 1
+  end
+  names
 end
 
 # call your method here!
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
